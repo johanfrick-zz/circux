2fd72b6432db3da320b196d578d5122c2fc5aee3
Jonas Hagberg <jonas.hagberg@edgeware.tv>
2013-02-04 09:55:30 +0100

    Added code for calculating md5 used for signing the auth req

--- circ.sh
+++ circ.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 # Bash script for communicating with http://getcirc.com/
 
+# calculate MD5 of request for signing
+#CLIENT_SECRET="ABC"
+#CLIENT_ID="53a6f338e42b3971821d71b6d04e79e5"
+#CODE="JDJ5JDA4JGJoMnllRWQ4OS9sTDhQdEppM1czYi5XSHJPOVNMWldpckFaS2FRR3E5TkdIdmFRN3Q2OHBx"
+#GRANT_TYPE="authorization_code"
+#REDIRECT_URI="https://example.com/authcode.php"
+#STATE="VGhlIEF2ZW5nZXJz"
+#CLIENT_SIG=`echo -n $CLIENT_SECRET"client_id"$CLIENT_ID"code"$CODE"grant_type"$GRANT_TYPE"redirect_uri"$REDIRECT_URI"state"$STATE | md5sum | awk '{ print $1 }'`
+#echo $CLIENT_SIG
+
+
 if [ $# -lt 2 ]
 then
 	echo
